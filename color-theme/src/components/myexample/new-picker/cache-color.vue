68a1df9d1fb846e607d440f35ef26630acc29fd7
minxr <minxr@fxiaoke.com>
2019-07-17 00:20:31 +0800

    feat:hsl色环

--- color-theme/src/components/myexample/new-picker/cache-color.vue
+++ color-theme/src/components/myexample/new-picker/cache-color.vue
@@ -1,8 +1,17 @@
 <template>
-  <el-popover placement="top" width="300" trigger="click">
+  <el-popover placement="top" width="400" trigger="click">
     <div class="cache-container">
-      <canvas id="mycanvas" ref="canvas" width="300" height="300"></canvas>
+      <canvas id="mycanvas" ref="canvas" width="200" height="300"></canvas>
     </div>
+    <div class="hsl-container">
+      <div class="hsl-container-top"></div>
+      <div class="hsl-container-form">
+        <div class="hsl-item">h:<input ></div>
+        <div class="hsl-item">s:<input ></div>
+        <div class="hsl-item">l:<input ></div>
+      </div>
+    </div>
+
     <el-button slot="reference" class="cache-button">
       <div class="cache-color"></div>
     </el-button>
@@ -14,7 +23,8 @@ export default {
   data(){
     return{
       canvas:null,
-      ctx:null
+      ctx:null,
+      hsl:{}
     }
   },
   methods: {
@@ -25,35 +35,57 @@ export default {
       this.canvas = this.$refs.canvas;
       //背景
       let ctx = this.canvas.getContext("2d");
+      this.ctx=ctx;
       let grd = ctx.createLinearGradient(0, 0, 0, 300);
       for(let i=1;i<=360;i++){
           grd.addColorStop((i/360).toFixed(4), this.gethsl(i));
       }
-      // grd.addColorStop(0,'hsl(0,100%,50%)');
-      // // grd.addColorStop(0.5,'#f0f');
-      // grd.addColorStop(1,'hsl(360,100%,50%)');
       ctx.fillStyle=grd;
-      ctx.fillRect(100, 0, 30, 300);
-     ctx.stroke();
+      ctx.fillRect(150, 0, 50, 300);
+      ctx.stroke();
     },
     gethsl(i){
       // console.log("hsl("+i+",100%,100%)")
       return "hsl("+i+",100%,50%)"
-
     },
-    // pick(e,ctx){
-    //   let x = e.layerX,
-		// 		y = e.layerY,
-		// 		pixel = ctx.getImageData(x, y, 1, 1),
-    //     data = pixel.data;
+    pick(e,ctx){
+      let x = e.layerX,
+				y = e.layerY,
+				pixel = ctx.getImageData(x, y, 1, 1),
+        data = pixel.data;
+        let rgb=[data[0],data[1],data[2]]
+        this.fromRgbtoHsl(rgb)
         
-    // }
+    },
+    fromRgbtoHsl(rgb){
+      let r=rgb[0],g=rgb[1],b=rgb[2];
+      r /= 255, g /= 255, b /= 255;
+
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+    this.hsl={h:Math.floor(h * 360),s:Math.floor(s * 100) + '%',l:Math.floor(l * 100) + '%'}
+    return 'hsl(' + Math.floor(h * 360) + ',' + Math.floor(s * 100) + '%,' + Math.floor(l * 100) + '%)';
+    }
   },
   mounted() {
     this.initCanvas();
-    // this.canvas.onmousemove = () => {
-		// 	this.pick(event, this.ctx);
-		// }
+    this.canvas.onmousemove = () => {
+			this.pick(event, this.ctx);
+		}
   }
 };
 </script>
@@ -71,7 +103,44 @@ export default {
   border: none;
 }
 .cache-container {
-  width: 100%;
+  width: 50%;
   height: 300px;
+  display: inline-block;
+}
+.hsl-container{
+  // display: inline-block;
+  float: right;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  .hsl-container-top{
+    width: 100%;
+    height: 50px;
+    background-color: pink;
+    margin-bottom: 10px;
+    border-radius: 5px;
+
+  }
+  .hsl-container-form{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    .hsl-item{
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      width: 50px;
+      padding: 0 8px;
+      // margin:0 10px;
+      input{
+        width: 50px;
+      }
+    }
+
+
+  }
+
 }
 </style>
