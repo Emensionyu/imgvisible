a76812d936f8d5eb32a0e05ac967755a419d8f06
Emensionyu <2833359326@qq.com>
2020-03-30 20:40:32 +0800

    optimizationoptimizationoptimizationoptimization 选项

--- webpack2-demo/readme.md
+++ webpack2-demo/readme.md
@@ -3,5 +3,12 @@
 > UglifyJSPlugin 代码压缩
 > HtmlWebpackPlugin 管理输出html文件
 > webapck 内置插件 DefinePlugin 定义环境变量
-
+> SplitChunksPlugin 移除重复模块。
 小提示 如何你是window系统，运行时指定环境请用npm run xxx set NODE_ENV=production
+## webpack配置选项
+>mode 
+>optimization
+## webapck loader
+>mini-css-extract-plugin：用于将 CSS 从主应用程序中分离。
+>bundle-loader：用于分离代码和延迟加载生成的 bundle。
+>promise-loader：类似于 bundle-loader ，但是使用了 promise API。
--- webpack2-demo/src/index.js
+++ webpack2-demo/src/index.js
@@ -1,5 +1,13 @@
-if (process.env.NODE_ENV === 'production') {
-  console.log("I am in pro");
-} else {
-  console.log("I am in dev");
-}
+import _ from 'lodash';
+
+function component() {
+    var element = document.createElement('pre');
+    var btn = document.createElement('button');
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+  }
+  
+   let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+   document.body.appendChild(element);
+  
